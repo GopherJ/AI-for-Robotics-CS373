07c13c5b4b09491094c32ae06ac5d248dddcf7a2
zhao-zh10 <46366974+zhao-zh10@users.noreply.github.com>
2019-04-24 22:43:23 +0800

    Submit Implement A-star Solution

--- Module4-Search_Path/Implement_A_star.py
+++ Module4-Search_Path/Implement_A_star.py
@@ -1,3 +1,4 @@
+# Author: zhao-zh10
 # -----------
 # User Instructions:
 #
@@ -36,7 +37,7 @@ delta = [[-1, 0],  # go up
 delta_name = ['^', '<', 'v', '>']
 
 
-def search(grid, init, goal, cost, heuristic):
+def search(grid, init, goal, cost, heuristic, debug_flag = False):
     # ----------------------------------------
     # modify the code below
     # ----------------------------------------
@@ -45,41 +46,94 @@ def search(grid, init, goal, cost, heuristic):
 
     expand = [[-1 for col in range(len(grid[0]))] for row in range(len(grid))]
     action = [[-1 for col in range(len(grid[0]))] for row in range(len(grid))]
+    policy = [[' ' for col in range(len(grid[0]))] for row in range(len(grid))]
 
     x = init[0]
     y = init[1]
     g = 0
+    h = heuristic[x][y]
+    f = g + h
 
-    open = [[g, x, y]]
+    open = [[f, g, h, x, y]]
 
     found = False  # flag that is set when search is complete
     resign = False  # flag set if we can't find expand
     count = 0
 
+    if debug_flag:
+        print('initial open list:')
+        for i in range(len(open)):
+            print("      ", open[i])
+        print("----")
     while not found and not resign:
         if len(open) == 0:
             resign = True
+            if debug_flag:
+                print('Fail')
+                print('###### Search terminated without success')
             return "Fail"
         else:
+            # remove node from list
             open.sort()
             open.reverse()
             next = open.pop()
-            x = next[1]
-            y = next[2]
-            g = next[0]
+            if debug_flag:
+                print('take list item')
+                print(next)
+            x = next[3]
+            y = next[4]
+            g = next[1]
             expand[x][y] = count
             count += 1
 
             if x == goal[0] and y == goal[1]:
+                if debug_flag:
+                    print(next)
+                    print('###### Search successful')
                 found = True
             else:
+                # expand winning element and add to new open list
                 for i in range(len(delta)):
                     x2 = x + delta[i][0]
                     y2 = y + delta[i][1]
-                    if x2 >= 0 and x2 < len(grid) and y2 >= 0 and y2 < len(grid[0]):
+                    if 0 <= x2 < len(grid) and 0 <= y2 < len(grid[0]):
                         if closed[x2][y2] == 0 and grid[x2][y2] == 0:
                             g2 = g + cost
-                            open.append([g2, x2, y2])
+                            h2 = heuristic[x2][y2]
+                            f2 = g2 + h2
+                            open.append([f2, g2, h2, x2, y2])
+                            if debug_flag:
+                                print('append list item')
+                                print([f2, g2, h2, x2, y2])
                             closed[x2][y2] = 1
+                            action[x2][y2] = i
+                if debug_flag:
+                    print('---'*10)
+                    print('new open list:')
+                    for i in range(len(open)):
+                        print('     ', open[i])
+                    print('---'*10)
 
+    if found:
+        x = goal[0]
+        y = goal[1]
+        policy[x][y] = '*'
+        while x != init[0] or y != init[0]:
+            x2 = x - delta[action[x][y]][0]
+            y2 = y - delta[action[x][y]][1]
+            policy[x2][y2] = delta_name[action[x][y]]
+            x = x2
+            y = y2
+
+        if debug_flag:
+            print('---'*10)
+            print('The path policy is:')
+            for m in range(len(policy)):
+                print(policy[m])
+            print('---'*10)
+            print('The expanded table is :')
+            for k in range(len(expand)):
+                print(expand[k])
     return expand
+
+search(grid, init, goal, cost, heuristic)

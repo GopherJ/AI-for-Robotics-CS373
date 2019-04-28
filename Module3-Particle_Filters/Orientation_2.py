0bcfeb206c6c5c94a1c509df39cde98f3d2993d7
zhao-zh10 <zhao-zh10@qq.com>
2019-04-28 15:43:16 +0800

    Submit Orientation_2 Solution

--- Module3-Particle_Filters/Orientation_2.py
+++ Module3-Particle_Filters/Orientation_2.py
@@ -1,3 +1,4 @@
+# Author: zhao-zh10
 # In this exercise, write a program that will
 # run your previous code twice.
 # Please only modify the indicated area below!
@@ -14,17 +15,17 @@ class robot:
         self.x = random.random() * world_size
         self.y = random.random() * world_size
         self.orientation = random.random() * 2.0 * pi
-        self.forward_noise = 0.0;
-        self.turn_noise = 0.0;
-        self.sense_noise = 0.0;
+        self.forward_noise = 0.0
+        self.turn_noise = 0.0
+        self.sense_noise = 0.0
 
     def set(self, new_x, new_y, new_orientation):
         if new_x < 0 or new_x >= world_size:
-            raise ValueError, 'X coordinate out of bound'
+            raise(ValueError, 'X coordinate out of bound')
         if new_y < 0 or new_y >= world_size:
-            raise ValueError, 'Y coordinate out of bound'
+            raise(ValueError, 'Y coordinate out of bound')
         if new_orientation < 0 or new_orientation >= 2 * pi:
-            raise ValueError, 'Orientation must be in [0..2pi]'
+            raise(ValueError, 'Orientation must be in [0..2pi]')
         self.x = float(new_x)
         self.y = float(new_y)
         self.orientation = float(new_orientation)
@@ -32,9 +33,9 @@ class robot:
     def set_noise(self, new_f_noise, new_t_noise, new_s_noise):
         # makes it possible to change the noise parameters
         # this is often useful in particle filters
-        self.forward_noise = float(new_f_noise);
-        self.turn_noise = float(new_t_noise);
-        self.sense_noise = float(new_s_noise);
+        self.forward_noise = float(new_f_noise)
+        self.turn_noise = float(new_t_noise)
+        self.sense_noise = float(new_s_noise)
 
     def sense(self):
         Z = []
@@ -46,7 +47,7 @@ class robot:
 
     def move(self, turn, forward):
         if forward < 0:
-            raise ValueError, 'Robot cant move backwards'
+            raise(ValueError, 'Robot cant move backwards')
 
             # turn, and add randomness to the turning command
         orientation = self.orientation + float(turn) + random.gauss(0.0, self.turn_noise)
@@ -74,7 +75,7 @@ class robot:
 
         # calculates how likely a measurement should be
 
-        prob = 1.0;
+        prob = 1.0
         for i in range(len(landmarks)):
             dist = sqrt((self.x - landmarks[i][0]) ** 2 + (self.y - landmarks[i][1]) ** 2)
             prob *= self.Gaussian(dist, self.sense_noise, measurement[i])
@@ -88,43 +89,47 @@ class robot:
 # myrobot.set_noise(5.0, 0.1, 5.0)
 # myrobot.set(30.0, 50.0, pi/2)
 # myrobot = myrobot.move(-pi/2, 15.0)
-# print myrobot.sense()
+# print(myrobot.sense())
 # myrobot = myrobot.move(-pi/2, 10.0)
-# print myrobot.sense()
+# print(myrobot.sense())
 
 ####   DON'T MODIFY ANYTHING ABOVE HERE! ENTER/MODIFY CODE BELOW ####
-myrobot = robot()
-myrobot = myrobot.move(0.1, 5.0)
-Z = myrobot.sense()
-
 N = 1000
+T = 2
+
+myrobot = robot()
+
 p = []
 for i in range(N):
     x = robot()
     x.set_noise(0.05, 0.05, 5.0)
     p.append(x)
 
-p2 = []
-for i in range(N):
-    p2.append(p[i].move(0.1, 5.0))
-p = p2
+for t in range(T):
+    myrobot = myrobot.move(0.1, 5.0)
+    Z = myrobot.sense()
 
-w = []
-for i in range(N):
-    w.append(p[i].measurement_prob(Z))
+    p2 = []
+    for i in range(N):
+        p2.append(p[i].move(0.1, 5.0))
+    p = p2
 
-p3 = []
-index = int(random.random() * N)
-beta = 0.0
-mw = max(w)
-for i in range(N):
-    beta += random.random() * 2.0 * mw
-    while beta > w[index]:
-        beta -= w[index]
-        index = (index + 1) % N
-    p3.append(p[index])
-p = p3
+    w = []
+    for i in range(N):
+        w.append(p[i].measurement_prob(Z))
 
-print p  # Leave this print statement for grading purposes!
+    p3 = []
+    index = int(random.random() * N)
+    beta = 0.0
+    mw = max(w)
+    for i in range(N):
+        beta += random.random() * 2.0 * mw
+        while beta > w[index]:
+            beta -= w[index]
+            index = (index + 1) % N
+        p3.append(p[index])
+    p = p3
+
+print(p)  # Leave this print statement for grading purposes!
 
 

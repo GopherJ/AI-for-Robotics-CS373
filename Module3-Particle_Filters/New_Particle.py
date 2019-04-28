249cd2603307225ae1b7fe1527e6ba2294ba7f63
zhao-zh10 <zhao-zh10@qq.com>
2019-04-28 15:16:36 +0800

    Submit New Particle Solution

--- Module3-Particle_Filters/New_Particle.py
+++ Module3-Particle_Filters/New_Particle.py
@@ -1,3 +1,4 @@
+# Author: zhao-zh10
 # In this exercise, try to write a program that
 # will resample particles according to their weights.
 # Particles with higher weights should be sampled
@@ -18,17 +19,17 @@ class robot:
         self.x = random.random() * world_size
         self.y = random.random() * world_size
         self.orientation = random.random() * 2.0 * pi
-        self.forward_noise = 0.0;
-        self.turn_noise = 0.0;
-        self.sense_noise = 0.0;
+        self.forward_noise = 0.0
+        self.turn_noise = 0.0
+        self.sense_noise = 0.0
 
     def set(self, new_x, new_y, new_orientation):
         if new_x < 0 or new_x >= world_size:
-            raise ValueError, 'X coordinate out of bound'
+            raise(ValueError, 'X coordinate out of bound')
         if new_y < 0 or new_y >= world_size:
-            raise ValueError, 'Y coordinate out of bound'
+            raise(ValueError, 'Y coordinate out of bound')
         if new_orientation < 0 or new_orientation >= 2 * pi:
-            raise ValueError, 'Orientation must be in [0..2pi]'
+            raise(ValueError, 'Orientation must be in [0..2pi]')
         self.x = float(new_x)
         self.y = float(new_y)
         self.orientation = float(new_orientation)
@@ -36,9 +37,9 @@ class robot:
     def set_noise(self, new_f_noise, new_t_noise, new_s_noise):
         # makes it possible to change the noise parameters
         # this is often useful in particle filters
-        self.forward_noise = float(new_f_noise);
-        self.turn_noise = float(new_t_noise);
-        self.sense_noise = float(new_s_noise);
+        self.forward_noise = float(new_f_noise)
+        self.turn_noise = float(new_t_noise)
+        self.sense_noise = float(new_s_noise)
 
     def sense(self):
         Z = []
@@ -50,7 +51,7 @@ class robot:
 
     def move(self, turn, forward):
         if forward < 0:
-            raise ValueError, 'Robot cant move backwards'
+            raise(ValueError, 'Robot cant move backwards')
 
             # turn, and add randomness to the turning command
         orientation = self.orientation + float(turn) + random.gauss(0.0, self.turn_noise)
@@ -78,7 +79,7 @@ class robot:
 
         # calculates how likely a measurement should be
 
-        prob = 1.0;
+        prob = 1.0
         for i in range(len(landmarks)):
             dist = sqrt((self.x - landmarks[i][0]) ** 2 + (self.y - landmarks[i][1]) ** 2)
             prob *= self.Gaussian(dist, self.sense_noise, measurement[i])
@@ -92,9 +93,9 @@ class robot:
 # myrobot.set_noise(5.0, 0.1, 5.0)
 # myrobot.set(30.0, 50.0, pi/2)
 # myrobot = myrobot.move(-pi/2, 15.0)
-# print myrobot.sense()
+# print(myrobot.sense())
 # myrobot = myrobot.move(-pi/2, 10.0)
-# print myrobot.sense()
+# print(myrobot.sense())
 
 myrobot = robot()
 myrobot = myrobot.move(0.1, 5.0)
@@ -122,5 +123,19 @@ for i in range(N):
 # Also, DO NOT MODIFY p.
 
 p3 = []
-
-
+sum_w = sum(w)
+w2 = []
+k = 0.0
+for i in range(N):
+    k += w[i]
+    w2.append(k)
+for i in range(N):
+    rand = random.random()
+    select = 0
+    for j in range(N):
+        if w2[j]/sum_w > rand:
+            select = j
+            break
+    p3.append(p[j])
+p = p3
+print(p)

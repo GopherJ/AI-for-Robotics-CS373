ece4dd8e13a56d23c93daa2d65d07bcd9857954c
zhao-zh10 <46366974+zhao-zh10@users.noreply.github.com>
2019-04-30 10:33:06 +0800

    Submit Sensing Solution

--- Module3-Particle_Filters/Sensing.py
+++ Module3-Particle_Filters/Sensing.py
@@ -1,3 +1,4 @@
+# Author: zhao-zh10
 # --------------
 # USER INSTRUCTIONS
 #
@@ -31,7 +32,9 @@ import random
 #
 # NOTE: Landmark coordinates are given in (y, x) form and NOT
 # in the traditional (x, y) format!
-
+# In this assignment, treat the world as an infinite grid: the robot can move so that its x- or y-coordinates
+# are larger in magnitude than the world_size variable.
+# (The world_size variable only affects the robot's initial coordinates.)
 landmarks = [[0.0, 100.0], [0.0, 0.0], [100.0, 0.0], [100.0, 100.0]]  # position of 4 landmarks in (y, x) form.
 world_size = 100.0  # world is NOT cyclic. Robot is allowed to travel "out of bounds"
 
@@ -58,8 +61,7 @@ class robot:
         self.distance_noise = 0.0  # initialize distance noise to zero
 
     def __repr__(self):
-        return '[x=%.6s y=%.6s orient=%.6s]' % (str(self.x), str(self.y),
-                                                str(self.orientation))
+        return '[x=%.6s y=%.6s orient=%.6s]' % (str(self.x), str(self.y),str(self.orientation))
 
     # --------
     # set:
@@ -68,7 +70,7 @@ class robot:
 
     def set(self, new_x, new_y, new_orientation):
         if new_orientation < 0 or new_orientation >= 2 * pi:
-            raise ValueError, 'Orientation must be in [0..2pi]'
+            raise(ValueError, 'Orientation must be in [0..2pi]')
         self.x = float(new_x)
         self.y = float(new_y)
         self.orientation = float(new_orientation)
@@ -92,11 +94,19 @@ class robot:
     #   obtains bearings from positions
     #
 
-    def sense(self):  # do not change the name of this function
+    def sense(self, add_noise=True):  # do not change the name of this function
         Z = []
 
         # ENTER CODE HERE
         # HINT: You will probably need to use the function atan2()
+        for i in range(len(landmarks)):
+            delta_x = landmarks[i][1] - self.x
+            delta_y = landmarks[i][0] - self.y
+            bearing = atan2(delta_y, delta_x) - self.orientation
+            if add_noise:
+                bearing += random.gauss(0.0, self.bearing_noise)
+            bearing %= 2.0 * pi
+            Z.append(bearing)
 
         return Z  # Leave this line here. Return vector Z of 4 bearings.
 
@@ -115,17 +125,17 @@ class robot:
 ## 1) The following code should print the list [6.004885648174475, 3.7295952571373605, 1.9295669970654687, 0.8519663271732721]
 ##
 ##
-##length = 20.
-##bearing_noise  = 0.0
-##steering_noise = 0.0
-##distance_noise = 0.0
-##
-##myrobot = robot(length)
-##myrobot.set(30.0, 20.0, 0.0)
-##myrobot.set_noise(bearing_noise, steering_noise, distance_noise)
-##
-##print 'Robot:        ', myrobot
-##print 'Measurements: ', myrobot.sense()
+# length = 20.
+# bearing_noise  = 0.0
+# steering_noise = 0.0
+# distance_noise = 0.0
+#
+# myrobot = robot(length)
+# myrobot.set(30.0, 20.0, 0.0)
+# myrobot.set_noise(bearing_noise, steering_noise, distance_noise)
+#
+# print('Robot:        ', myrobot)
+# print('Measurements: ', myrobot.sense())
 ##
 
 ## IMPORTANT: You may uncomment the test cases below to test your code.
@@ -138,17 +148,17 @@ class robot:
 ## 2) The following code should print the list [5.376567117456516, 3.101276726419402, 1.3012484663475101, 0.22364779645531352]
 ##
 ##
-##length = 20.
-##bearing_noise  = 0.0
-##steering_noise = 0.0
-##distance_noise = 0.0
-##
-##myrobot = robot(length)
-##myrobot.set(30.0, 20.0, pi / 5.0)
-##myrobot.set_noise(bearing_noise, steering_noise, distance_noise)
-##
-##print 'Robot:        ', myrobot
-##print 'Measurements: ', myrobot.sense()
+# length = 20.
+# bearing_noise  = 0.0
+# steering_noise = 0.0
+# distance_noise = 0.0
+#
+# myrobot = robot(length)
+# myrobot.set(30.0, 20.0, pi / 5.0)
+# myrobot.set_noise(bearing_noise, steering_noise, distance_noise)
+#
+# print('Robot:        ', myrobot)
+# print('Measurements: ', myrobot.sense())
 ##
 
 
